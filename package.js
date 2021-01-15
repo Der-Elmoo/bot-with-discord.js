dafcaa7436c3dcb309af5d38651554d320dc48de
Der Elmo <56122564+Der-Elmoo@users.noreply.github.com>
2021-01-15 10:58:09 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,42 @@
+{
+  "//1": "describes your app and its dependencies",
+  "//2": "https://docs.npmjs.com/files/package.json",
+  "//3": "updating this file will download and update your packages",
+  "name": "hello-express",
+  "version": "0.0.1",
+  "description": "A simple Node app built on Express, instantly up and running.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "discord.js": "^12.5.1",
+    "dotenv": "^8.2.0",
+    "ascii-table": "^0.0.9",
+    "fs": "^0.0.2",
+    "quick.db": "^7.1.2",
+    "axios": "^0.21.1",
+    "novelcovid": "^3.0.0",
+    "imdb-api": "^4.4.0",
+    "parse-ms": "^2.1.0",
+    "twemoji-parser": "^13.0.0",
+    "figlet": "^1.5.0",
+    "moment": "^2.29.1",
+    "google-play-scraper": "^8.0.2",
+    "node-fetch": "^2.6.1",
+    "weather-js": "^2.0.0"
+  },
+  "engines": {
+    "node": "12.x"
+  },
+  "repository": {
+    "url": "https://glitch.com/edit/#!/hello-express"
+  },
+  "license": "MIT",
+  "keywords": [
+    "node",
+    "glitch",
+    "express"
+  ]
+}
